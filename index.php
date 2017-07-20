6d22c9ce672380005684d405692dacd412691421
RômuloSousa <romulo.rmos@hotmail.com>
2017-07-19 23:59:25 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,395 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html lang="pt-br"  xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <title>Sulnet </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=no">
+    <link href="https://fonts.googleapis.com/css?family=Rajdhani:400,500,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" type="text/css">
+    <script src="https://use.fontawesome.com/7f02b4d775.js"></script>
+<!--    <link rel="stylesheet" href="http://www.tcheturbo.com.br/app/css/style.css"/>-->
+    <link rel="stylesheet" href="css/style.css"/>
+
+    <link rel='icon' href='midia/favicon.png'/>
+<!--    <link rel='icon' href='http://www.tcheturbo.com.br/app/midia/favicon.png'/>-->
+<!--    <meta http-equiv="pragma" content="no-cache" />-->
+<!--    <meta name="description" content="" />-->
+<!--    <meta name="keywords" content="tcheturbo, internet" />-->
+<!--    <meta name="robots" content="follow" />-->
+<!--    <meta name="rating" content="general" />-->
+<!--    <meta name="author" content="Orby Internet e Design" />-->
+<!--    <meta name="language" content="pt-br" />-->
+<!---->
+<!--    <meta name='so:host' content='http://www.tcheturbo.com.br/' />-->
+<!--    <meta name='so:urlMidia' content='http://www.tcheturbo.com.br/app/midia/' />-->
+<!--    <meta name='so:urlCSS' content='http://www.tcheturbo.com.br/app/css/' />-->
+<!--    <meta name='so:urlJS' content='http://www.tcheturbo.com.br/app/js/' />-->
+<!--    <meta name='so:urlHELPERS' content='app/helpers/' />-->
+<!--    <meta name='so:controller' content='index' />-->
+<!--    <meta name='so:action' content='inicio' />-->
+
+    <!-- Page MENU TOPO -->
+    <div class="topo bg-azul">
+
+        <div class="col-md-6 col-sm-12">
+            <a href="http://www.tcheturbo.com.br/planos/para_voce_urbano" class="amarelo plano-topo">
+                Para Você Urbano
+            </a> <span> | </span>
+            <a href="http://www.tcheturbo.com.br/planos/para_voce_rural" class="verde plano-topo">
+                Para Você Rural
+            </a> <span> | </span>
+            <a href="http://www.tcheturbo.com.br/planos/para_sua_empresa" class="vermelho plano-topo">
+                Para Sua Empresa
+            </a>
+
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <a class="youtube externo pull-right" href="#">
+                <i class="fa fa-youtube" aria-hidden="true"></i>
+            </a>
+            <a class="facebook externo pull-right" href="#">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+
+            <a class="envelope pull-right" href="#" style="padding: 0 15px">
+                <i class="fa fa-envelope-o" aria-hidden="true"></i>
+
+            </a>
+            <div class="clearfix"></div>
+
+        </div>
+
+
+    </div>
+
+    <!-- Navigation -->
+    <nav class="navbar menuNavBar" role="navigation">
+
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header col-lg-2 col-md-4">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menutopo">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php" title="Sulnet">
+                <img src="imagens/logo-tema-padrao.png" alt="Tchê Turbo" class="img-responsive">
+            </a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="col-lg-10 col-md-8">
+
+            <div class="collapse navbar-collapse" id="menutopo">
+                <ul class="nav navbar-nav col-xs-12">
+                    <li>
+                        <a href="#">Nossa Empresa</a>
+                    </li>
+                    <li>
+                        <a href="#">Serviços</a>
+                    </li>
+                    <li>
+                        <a href="#">Planos</a>
+                    </li>
+                    <li>
+                        <a href="#">Área de Cobertura</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Contato</a>
+                    </li>
+
+
+                    <li class="pull-right">
+                        <a href="#" class="externo">
+                            <i class="icone webmail"></i><strong>Área do Cliente</strong></a>
+                    </li>
+
+
+
+                </ul>
+            </div>
+        </div>
+        <!-- /.navbar-collapse -->
+
+    </nav>
+    <div id="myCarousel" class="carousel slide banners" data-ride="carousel">
+
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+
+
+            <a href="#" class="item active fill banner-institucional" style="background-image: url(midia/bg-institucional.jpg)">
+                <div class="container">
+                    <div class="col-md-4 col-md-offset-1 meio">
+                        <img src="midia/institucional-banner.png" class="img-responsive">
+                    </div>
+                </div>
+            </a>
+
+            <a href="#" class="item fill" style="background-image: url(midia/bg-para-sua-empresa.jpg)">
+                <div class="container">
+                    <div class="col-md-4 col-md-offset-1 meio">
+                        <img src="midia/para-sua-empresa-branco.png" class="img-responsive">
+                    </div>
+                    <div class="col-md-5 col-md-offset-1 ">
+                        <div class="text-banner meio text-right">
+                            <div style="height: auto">
+                                <strong>Soluções de alta performance</strong><br>
+                                para qualquer situação.<br />
+                                <span class="saiba-mais-banner bg-vermelho">saiba mais</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="http://www.tcheturbo.com.br/" class="item fill" style="background-image: url(midia/bg-para-voce-urbano.jpg)">
+                <div class="container">
+                    <div class="col-md-4 col-md-offset-1 meio">
+                        <img src="midia/para-voce-urbano-branco.png" class="img-responsive">
+                    </div>
+                    <div class="col-md-5 col-md-offset-1 ">
+                        <div class="text-banner meio text-right">
+                            <div style="height: auto">
+                                <strong>Abrace todas as possibilidades</strong><br>
+                                que a tecnologia pode oferecer.<br />
+                                <span class="saiba-mais-banner bg-amarelo">saiba mais</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+
+
+            <a href="http://www.tcheturbo.com.br/" class="item fill" style="background-image: url(midia/bg-para-voce-rural.jpg)">
+                <div class="container">
+                    <div class="col-md-4 col-md-offset-1 meio">
+                        <img src="midia/para-voce-rural-branco.png" class="img-responsive">
+                    </div>
+                    <div class="col-md-5 col-md-offset-1 ">
+                        <div class="text-banner meio text-right">
+                            <div style="height: auto">
+                                <strong>Não importa a distância,  </strong><br>
+                                a gente vai até você.<br />
+                                <span class="saiba-mais-banner bg-verde">saiba mais</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <i class="fa fa-angle left" aria-hidden="true"></i>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <i class="fa fa-angle-right" aria-hidden="true"></i>
+            <span class="sr-only">Next</span>
+        </a>
+
+    </div>
+
+    <div class="divisor">
+        <div class="col-md-4 bg-verde"></div>
+        <div class="col-md-4 bg-vermelho"></div>
+        <div class="col-md-4 bg-amarelo"></div>
+    </div>
+    <!-- Ícones -->
+    <div class="planosHome">
+
+        <div class="container">
+
+            <div class="titulo azul text-center">
+            <span>
+                Conheça nossos <strong>Serviços</strong>
+            </span>
+            </div>
+
+            <div class="col-md-4 text-center azul" >
+                <div class="row">
+                    <img src="midia/para-voce-urbano.png" class="img-responsive"/>
+                    Abrace todas as possibilidades que a <br/>tecnologia pode oferecer.
+                    <a href="#" class="saiba-mais-full bg-amarelo">saiba mais sobre este serviço</a>
+                </div>
+            </div>
+            <div class="col-md-4 text-center azul" >
+                <div class="row">
+                    <img src="midia/para-voce-rural.png" class="img-responsive"/>
+                    Não importa a distância, a gente <br/>
+                    vai até você.
+                    <a href="#" class="saiba-mais-full bg-verde">saiba mais sobre este serviço</a>
+                </div>
+            </div>
+            <div class="col-md-4 text-center azul" >
+                <div class="row">
+                    <img src="midia/para-sua-empresa.png" class="img-responsive"/>
+                    Soluções de alta performance para <br/>
+                    qualquer situação.
+                    <a href="#" class="saiba-mais-full bg-vermelho">saiba mais sobre este serviço</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="servicosHome">
+
+        <div class="container">
+
+            <div class="col-md-12">
+                <div class="titulo verde text-center">
+                <span>
+                    outros <strong>serviços</strong>
+                </span>
+                </div>
+            </div>
+            <div class="col-md-4 text-center" >
+                <img src="midia/hospedagem.png" class="img-responsive"/>
+                <h3 class="azul">HOSPEDAGEM</h3>
+                Sua empresa sempre online e acessível
+                <a href="#" class="saiba-mais-box centered"></a>
+            </div>
+
+            <div class="col-md-4 text-center" >
+                <img src="midia/backup.png" class="img-responsive"/>
+                <h3 class="azul">BACKUP</h3>
+                Armazene seus dados com total segurança
+                <a href="#" class="saiba-mais-box centered"></a>
+            </div>
+            <div class="col-md-4 text-center" >
+                <img src="midia/cloud.png" class="img-responsive"/>
+                <h3 class="azul">CLOUD</h3>
+                Seus arquivos acessíveis de qualquer local
+                <a href="#" class="saiba-mais-box centered"></a>
+            </div>
+        </div>
+        <br/><br/><br/>
+    </div>
+
+    <div class="area-atuacao">
+
+        <div class="container">
+
+            <div class="arrow-down"></div>
+
+            <div class="col-md-6 text-center">
+                <img class="img-responsive" src="midia/area-atuacao.png" />
+
+            </div>
+
+
+            <div class="col-md-5 col-md-offset-1">
+                <h2>Área de <strong>Atuação</strong></h2>
+
+                <p>
+                    Para nós, conectar é encurtar distâncias. Por isso, dispomos de moderna infraestrutura, distribuída em uma ampla área que abrange mais de 30 cidades no Rio Grande do Sul.
+
+                </p>
+
+                <div class="verde">
+                    <a href="#" class="saiba-mais-full bg-verde text-center">Saiba mais sobre as regiões que atendemos</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+    <div class="divisor">
+        <div class="col-md-4 bg-verde"></div>
+        <div class="col-md-4 bg-vermelho"></div>
+        <div class="col-md-4 bg-amarelo"></div>
+
+    </div>
+
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="imagens/logo-tema-padrao.png" class="brand"/>
+                    Desde 2003, aproximamos pessoas por meio de serviços de alta qualidade, oferecendo soluções inteligentes para as mais variadas necessidades.
+
+                    <br /><br />
+                    <a href="#" class="externo bg-amarelo social facebook">
+                        <i class="fa fa-facebook" aria-hidden="true"></i>
+                    </a>
+                    <a href="#" class="externo bg-amarelo social youtube">
+                        <i class="fa fa-youtube" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <h2 class="verde">Institucional           </h2>
+                    <a href="#"><i class="icone arrow-right"></i>Quem Somos</a><br/>
+                    <a href="$"><i class="icone arrow-right"></i>Área de Atuação</a><br/>
+
+                </div>
+                <div class="col-md-3">
+                    <h2 class="verde">Serviços           </h2>
+                    <a href="#"><i class="icone arrow-right"></i>Para Você Urbano</a><br/>
+                    <a href="#"><i class="icone arrow-right"></i>Para Você Rural</a><br/>
+                    <a href="#"><i class="icone arrow-right"></i>Para Sua Empresa</a><br/>
+                    <a href="#"><i class="icone arrow-right"></i>Hospedagem de Sites</a><br/>
+                    <a href="#"><i class="icone arrow-right"></i>Backup</a><br/>
+
+                </div>
+                <div class="col-md-3">
+                    <h2 class="vermelho">Entre em Contato           </h2>
+
+                    <div class="row">
+                        <div class="col-md-3">
+                            <i class="icone mark bg-vermelho"></i>
+                        </div>
+                        <div class="col-md-9">
+                            <span class="vermelho">Matriz</span><br/>
+                            <strong>(55) 3199-9150</strong><br/>
+                            Rua Presidente Kennedy, 897 - Sala 01G, Centro, Frederico Westphalen-RS
+                        </div>
+                        <div class="clear-fix"></div>
+                    </div>
+                    <div class="row" style="margin-top: 15px">
+                        <div class="col-md-3" >
+                            <i class="icone mark bg-vermelho"></i>
+                        </div>
+                        <div class="col-md-9">
+                            <span class="vermelho">Filial</span><br/>
+                            <strong>(55) 2121 0000</strong><br/>
+                            Rua Francisco Pinheiro, 332 - Sala 01, Centro, Palmeira
+                            das Missões-RS
+                        </div>
+                        <div class="clear-fix"></div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    2017 Todos os Direitos Reservados - Sulnet </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="http://maps.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
+    <script src="http://www.tcheturbo.com.br/app/js/uteis.js"></script>
+
+
+
+    </body>
+
+</html>
