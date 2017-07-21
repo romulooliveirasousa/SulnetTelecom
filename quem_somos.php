90e19f19d1231ae93a92e171f0c2db3f94ce8f74
Romulo <romulo.rmos@hotmail.com>
2017-07-21 11:54:39 -0300

    Inclusão das Páginas PHP

---
+++ quem_somos.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" class="wf-fontawesome-n4-active wf-active" lang="pt-br">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <title>Sulnet </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=no">
+    <link href="css/css.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <script src="js/webfontloader.js" style=""></script><script src="js/7f02b4d775.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel='icon' href='midia/ico.png'/>
+
+
+    <!-- Page MENU TOPO -->
+    <link rel="stylesheet" href="css/7f02b4d775.css" media="all">
+    <script type="text/javascript" charset="UTF-8" src="css/common.js"></script>
+    <script type="text/javascript" charset="UTF-8" src="js/util.js"></script>
+    <script type="text/javascript" charset="UTF-8" src="js/stats.js"></script>
+</head>
+<body>
+<div class="topo bg-azul">
+    <div class="col-md-6 col-sm-12">
+        <a href="#" class="amarelo plano-topo">
+            Para Você Urbano
+        </a> <span> | </span>
+        <a href="#" class="verde plano-topo">
+            Para Você Rural
+        </a> <span> | </span>
+        <a href="#" class="vermelho plano-topo">
+            Para Sua Empresa
+        </a>
+    </div>
+    <div class="col-md-6 col-sm-12">
+        <a class="youtube externo pull-right" href="#">
+            <i class="fa fa-youtube" aria-hidden="true"></i>
+        </a>
+        <a class="facebook externo pull-right" href="#">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+        </a>
+        <a class="envelope pull-right" href="#" style="padding: 0 15px">
+            <i class="fa fa-envelope-o" aria-hidden="true"></i>
+        </a>
+        <div class="clearfix"></div>
+    </div>
+</div>
+
+
+<!-- Navigation -->
+<nav class="navbar menuNavBar" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header col-lg-2 col-md-4">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menutopo">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php" title="Sulnet">
+            <img src="imagens/logo-tema-padrao.png" alt="Sulnet" class="img-responsive">
+        </a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="col-lg-10 col-md-8">
+        <div class="collapse navbar-collapse" id="menutopo">
+            <ul class="nav navbar-nav col-xs-12">
+                <li>
+                    <a href="quem_somos.htm">Nossa Empresa</a>
+                </li>
+                <li>
+                    <a href="#">Serviços</a>
+                </li>
+                <li>
+                    <a href="#">Planos</a>
+                </li>
+                <li>
+                    <a href="atuacao.htm">Área de Cobertura</a>
+                </li>
+
+                <li>
+                    <a href="contato.php">Contato</a>
+                </li>
+                <li class="pull-right">
+                    <a href="http://www.sntprovedores.com.br/central.php" class="externo">
+                        <i class="icone webmail"></i><strong>Área do Cliente</strong></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- /.navbar-collapse -->
+
+</nav>
+<div class="breadcrumb">
+    <nav class="container">
+        <a href="index.php" title="Home">Home</a>
+        <i class="fa fa-angle-right" aria-hidden="true"></i>
+        Quem Somos
+    </nav>
+</div>
+
+<div class="box-titulo fill" style="background-image:url('midia/titulo-contato.jpg');">
+    <div class="container text-center">
+        <div class="meio">
+            <h1>Quem Somos</h1>
+        </div>
+    </div>
+</div>
+<div class="divisor">
+    <div class="col-md-4 bg-amarelo"></div>
+    <div class="col-md-4 bg-verde"></div>
+    <div class="col-md-4 bg-vermelho"></div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 conteudo">
+            <h1 class="titulo vermelho">
+                <span>Quem Somos</span>
+            </h1>
+            <h4></h4>
+            <div>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;Somos um ISP que Atende Clientes Residênciais e Empresariais
+                    na região do Vale do Araguaia,Fornecendo um Serviço de Qualidade, com Rapidez e Responsabilidade.
+                    Trabalhamos com os melhores equipamentos e com a melhor equipe técnica,
+                    para oferecermos aos clientes serviços de qualidade com mais segurança e
+                    conforto ao navegar por esse mundo de informações. &nbsp;&nbsp;
+                </p>
+                <div>
+                    <br>
+                </div>
+                <div>
+                    <h3>MISSÃO</h3>
+                    <div>
+                        <br></div>
+                    <div>
+                        &nbsp;&nbsp;&nbspSer Uma Empresa Reconhecida no Vale do Araguaia Como Uma das
+                        Melhores Empresas Prestadoras de Serviços de Internet.
+                    </div>
+
+                    <div>
+                        <span style="color: inherit; font-size: 23px;"><br></span>
+                    </div>
+                    <h3>
+                        <span style="color: inherit; font-size: 23px;">VISÃO</span>
+                    </h3>
+                    <div>
+                        <br>
+                    </div>
+                    <div>
+                        &nbsp;&nbsp;&nbsp;&nbsp;Ser o Melhor Provedor de Internet do Vale do Araguaia,
+                        Oferecendo a seus Clientes maior Qualidade em Serviço de Banda Larga e Atendimento.&nbsp;
+                    </div>
+
+                    <div>
+                        <br>
+                    </div>
+                    <h3>VALORES</h3>
+                    <div>
+                        <br>
+                    </div>
+                    <div>
+                        &nbsp;&nbsp;&nbsp;&nbsp;- Compromisso e Qualidade Com o Cliente.
+                    </div>
+                    <div>
+                        &nbsp;&nbsp;&nbsp;&nbsp;- Melhorias em nossa infraestrutura.
+                    </div>
+                    <div>
+                        &nbsp;&nbsp;&nbsp;&nbsp;- Valorização da Nossa Equipe.
+                    </div>
+                    <div>
+                        &nbsp;&nbsp;&nbsp;&nbsp;- Inovacões em Novas Tecnologias Avançadas.
+                    </div></div>
+                <div>
+                    <br>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <div class="col-md-3 conteudo planosHome">
+            <div class="titulo azul">
+            <span>
+               nossos <strong>serviços</strong>
+            </span>
+            </div>
+            <div class="text-center azul">
+                <img src="midia/para-voce-urbano.png" class="img-responsive">
+                Abrace todas as possibilidades que a <br>tecnologia pode oferecer.
+                <a href="#" class="saiba-mais-full bg-amarelo">saiba mais</a>
+            </div>
+            <div class="text-center azul">
+                <img src="midia/para-voce-rural.png" class="img-responsive">
+                Não importa a distância, a gente <br>
+                vai até você.
+                <a href="#" class="saiba-mais-full bg-verde">saiba mais</a>
+            </div>
+            <div class="text-center azul">
+                <img src="midia/para-sua-empresa.png" class="img-responsive">
+                Soluções de alta performance para <br>
+                qualquer situação.
+                <a href="#" class="saiba-mais-full bg-vermelho">saiba mais</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="lightbox" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <button type="button" class="close hidden" data-dismiss="modal" aria-hidden="true">×</button>
+        <div class="modal-content">
+            <div class="modal-body">
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="divisor">
+    <div class="col-md-4 bg-verde"></div>
+    <div class="col-md-4 bg-vermelho"></div>
+    <div class="col-md-4 bg-amarelo"></div>
+</div>
+
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <img src="imagens/logo-tema-padrao.png" class="brand"/>
+                Desde 2003, aproximamos pessoas por meio de serviços de alta qualidade, oferecendo soluções inteligentes para as mais variadas necessidades.
+
+                <br /><br />
+                <a href="#" class="externo bg-amarelo social facebook">
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                </a>
+                <a href="#" class="externo bg-amarelo social youtube">
+                    <i class="fa fa-youtube" aria-hidden="true"></i>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <h2 class="verde">Institucional           </h2>
+                <a href="#"><i class="icone arrow-right"></i>Quem Somos</a><br/>
+                <a href="atuacao.htm"><i class="icone arrow-right"></i>Área de Atuação</a><br/>
+
+            </div>
+            <div class="col-md-3">
+                <h2 class="verde">Serviços           </h2>
+                <a href="#"><i class="icone arrow-right"></i>Para Você Urbano</a><br/>
+                <a href="#"><i class="icone arrow-right"></i>Para Você Rural</a><br/>
+                <a href="#"><i class="icone arrow-right"></i>Para Sua Empresa</a><br/>
+
+
+            </div>
+            <div class="col-md-3">
+                <h2 class="vermelho">Entre em Contato           </h2>
+
+                <div class="row">
+
+                    <div class="col-md-9">
+
+                        <strong>(63) 3602 - 1385</strong><br
+                    </div>
+
+                    Rua Barão do Rio Branco Nº 554
+                    Paraíso do Tocantins - TO
+                    <div class="clear-fix"></div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="copyright">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                2017 Todos os Direitos Reservados - Sulnet </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="http://maps.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
+<script src="js/uteis.js"></script>
+
+
+
+</body>
+
+</html>
