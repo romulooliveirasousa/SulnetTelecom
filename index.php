56841ea95c8286c107ca4e8f0023cf6aca380d09
romulo <romulo.rmos@hotmail.com>
2018-04-10 12:21:09 -0300

    teste de comit debian

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html lang="pt-br"  xmlns="http://www.w3.org/1999/xhtml">
-
+<!--TESTANDO COMIT DEBIAN -->
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
     <title>SulnetTelecom </title>
